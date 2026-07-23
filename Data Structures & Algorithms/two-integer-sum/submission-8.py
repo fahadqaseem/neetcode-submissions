7a120eed5556dcc51148f218a30ffc829cb132e6
Fahad Qaseem Khawar <fahad.qaseeem@gmail.com>
2026-07-22 23:37:15 -0400

    Add: two-integer-sum - submission-8

---
+++ Data Structures & Algorithms/two-integer-sum/submission-8.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
+
+        for i , n in enumerate(nums):
+            dif = target - n
+            if dif in seen:
+                return [seen[dif],i]
+            seen[n] = i
+        return
